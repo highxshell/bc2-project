eb6282bfd7c1c2963f4f434ba3b916d5299209ae
Artem Popov <121538758+highxshell@users.noreply.github.com>
2024-05-28 10:26:01 +0500

    added rust part of project

---
+++ SmartContract/constant.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+pub const USER_TAG : &[u8] = b"USER_STATE";
+
+#[constant]
+pub const ADD_FRIEND : &[u8] = b"FRIEND_STATE";
+
+#[constant]
+pub const STATUS_TAG : &[u8] = b"STATUS_STATE";
+
+#[constant]
+pub const VIDEO_TAG : &[u8] = b"VIDEO_STATE";
+
+#[constant]
+pub const ADD_LIKE : &[u8] = b"LIKE_STATE"
